759181bbdfd7fd2822a04a87fba108a9b9478075
Darren Morby <nullpointersetc@users.noreply.github.com>
2021-12-23 20:23:27 -0500

    Fourth version of TwoThreeTrees.v
    Add multilevel to two-three trees.

--- TwoThreeTrees/TwoThreeTrees.v
+++ TwoThreeTrees/TwoThreeTrees.v
@@ -7,72 +7,67 @@ Module TwoThreeTrees.
 		(Values : Coq.Structures.Equalities.Typ)
 		<: Coq.Structures.Equalities.Typ.
 
+		Inductive node_t : Type
+			:= binary_node
+				: node_t
+				-> Keys.t -> Values.t
+				-> node_t -> node_t
+			| ternary_node
+				: node_t
+				-> Keys.t -> Values.t
+				-> node_t
+				-> Keys.t -> Values.t
+				-> node_t -> node_t
+			| singleton_leaf
+				: Keys.t -> Values.t -> node_t
+			| doubleton_leaf
+				: Keys.t -> Values.t
+				-> Keys.t -> Values.t -> node_t.
+
 		Inductive tree_t : Type
-			:= empty_node
+			:= empty_tree
 				: tree_t
-			|  binary_node
-				: tree_t
-				-> Keys.t -> Values.t
-				-> tree_t
-				-> tree_t
-			|  ternary_node
-				: tree_t
-				-> Keys.t -> Values.t
-				-> tree_t
-				-> Keys.t -> Values.t
-				-> tree_t
-				-> tree_t.
+			| singleton_tree
+				: Keys.t -> Values.t -> tree_t
+			| doubleton_tree
+				: Keys.t -> Values.t
+				-> Keys.t -> Values.t -> tree_t
+			| binary_root
+				: node_t -> Keys.t -> Values.t
+				-> node_t -> tree_t
+			| ternary_root
+				: node_t -> Keys.t -> Values.t
+				-> node_t -> Keys.t -> Values.t
+				-> node_t -> tree_t.
 
 		Definition t := tree_t.
 
-		Definition empty := empty_node.
+		Definition empty := empty_tree.
 
 		Fixpoint insert (k : Keys.t)
 			(v : Values.t)
 			(t : tree_t)
 			: tree_t
-			:= (*1*) match t
-			with empty_node
-			=> binary_node (t) (k) (v) (t)
-			| binary_node (_ as t1)
-				(_ as k1) (_ as v1) (_ as t2)
-			=> (*2*) match Keys.ltb(k)(k1)
-				with true
-				=> binary_node(insert k v t1)(k1)(v1)(t2)
-				| false
-				=> (*3*) match Keys.ltb(k1)(k)
-				with true
-				=> binary_node(t1)(k1)(v1)(insert k v t2)
-				| false
-				=> binary_node(t1)(k)(v)(t2)
-				(*3*) end (*2*) end
-			| ternary_node (_ as t1)
-				(_ as k1) (_ as v1) (_ as t2)
-				(_ as k2) (_ as v2) (_ as t3)
-			=> (*4*) match Keys.ltb(k)(k1)
-				with true
-				=> ternary_node(insert k v t1)
-					(k1)(v1)(t2)(k2)(v2)(t3)
-				| false
-				=> (*5*) match Keys.ltb(k1)(k)
-				with true
-				=> (*6*) match Keys.ltb(k2)(k)
-					with true
-					=> ternary_node(t1)(k1)(v1)
-						(insert k v t2)(k2)(v2)(t3)
-					| false
-					=> (*7*) match Keys.ltb(k)(k2)
-					with true
-					=> ternary_node (t1)(k1)(v1)
-						(t2)(k2)(v2)(insert k v t3)
-					| false =>
-						ternary_node(t1)(k1)(v1)
-							(t2)(k)(v)(t3)
-					(*7*) end (*6*) end
-				| false
-				=> ternary_node(t1)(k)(v)(t2)(k2)(v2)(t3)
-				(*5*) end (*4*) end
-			(*1*) end.
+			:= match t
+			with empty_tree
+				=> singleton_tree (k) (v)
+			| singleton_tree (k1) (v1)
+				=> match Keys.ltb(k)(k1)
+				with true => doubleton_tree (k) (v) (k1) (v1)
+				| false => match Keys.ltb(k1)(k)
+				with true => doubleton_tree (k1) (v1) (k) (v)
+				| false => singleton_tree (k) (v)
+				end end.
+			| doubleton_tree (k1) (v1) (k2) (v2)
+				=> binary_root
+					(singleton_leaf k1 v1)
+					(k) (v)
+					(singleton_leaf k2 v2)
+			| binary_root (ln) (k1) (v1) (rn)
+				=> empty_tree
+			| ternary_root (ln) (k1) (v1) (mn) (k2) (v2) (rn)
+				=> empty_tree
+			end.
 
 	End Trees.
 
