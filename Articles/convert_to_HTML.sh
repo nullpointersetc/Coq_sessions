e834333be4397d41742be242034bd7e59be9d564
Darren Morby <nullpointersetc@users.noreply.github.com>
2022-08-12 02:29:12 -0400

    Add convert script

--- Articles/convert_to_HTML.sh
+++ Articles/convert_to_HTML.sh
@@ -1,1 +1,12 @@
-coqdoc --utf8 --html --parse-comments --short --files-from _CoqProject
+coqc Article_1_Playing_with_a_proof_assistant.v
+coqc Article_2_A_Little_Sentential_Calculus.v
+coqc Article_3_Making_it_generic.v
+coqc Article_4_Types.v
+coqc Article_5_Types_in_other_types.v
+coqc Article_6_Lists_of_things.v
+coqdoc --utf8 --html --parse-comments --short Article_1_Playing_with_a_proof_assistant.v
+coqdoc --utf8 --html --parse-comments --short Article_2_A_Little_Sentential_Calculus.v
+coqdoc --utf8 --html --parse-comments --short Article_3_Making_it_generic.v
+coqdoc --utf8 --html --parse-comments --short Article_4_Types.v
+coqdoc --utf8 --html --parse-comments --short Article_5_Types_in_other_types.v
+coqdoc --utf8 --html --parse-comments --short Article_6_Lists_of_things.v
